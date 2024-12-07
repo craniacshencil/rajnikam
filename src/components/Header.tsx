59135c054ccc94172e2fa3156ed8f33d2d465a96
craniacshencil <rajnikamrajnikam@gmail.com>
2024-12-07 22:14:39 +0530

    Fix rerouting to home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,7 +16,7 @@ const Header: React.FC<headerInputs> = ({ activeTab }) => {
             ),
             "hover:text-secondary transition duration-300",
           )}
-          href={`/`}
+          href={`/rajnikam`}
         >
           Home
         </a>
