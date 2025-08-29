343a9c8610b59a6729d2e651b439e36d8dcef2cf
craniacshencil <rajnikamrajnikam@gmail.com>
2025-08-29 21:10:57 +0530

    Update deprecated prism-syntax-highlighting package
    - Fix build process errors by doing this

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import path from "path";
 import mdx from "@mdx-js/rollup";
 import react from "@vitejs/plugin-react";
-import rehypePrism from "@mapbox/rehype-prism";
+import rehypePrism from "rehype-prism-plus";
 
 // https://vite.dev/config/
 export default defineConfig({
